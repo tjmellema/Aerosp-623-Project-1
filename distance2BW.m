19b8f5d4bc626e67ab61529bbf819e5ea2c7caf2
tjmellema <115052434+tjmellema@users.noreply.github.com>
2026-01-30 17:43:54 -0500

    Delete distance2BW.m

--- distance2BW.m
+++
@@ -1,9 +0,0 @@
-%Determines the distance to all the boundary cooridnates to for projection
-%
-%point_cord = potential point to be added
-%wall_cords = Nx2 vector of all boundary wall cords in order
-%wall_distances = Nx1 vector of distances to all boundary wall cords
-function wall_distances = distance2BW(point_cord, wall_cords)
-    diffs = wall_cords - point_cord;
-    wall_distances = sqrt(sum(diffs.^2, 2));
-end
