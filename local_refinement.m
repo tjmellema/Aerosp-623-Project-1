e6e58badd09299092596c5708af6a44bb3fd92f5
tjmellema <115052434+tjmellema@users.noreply.github.com>
2026-01-29 22:39:09 -0500

    Add files via upload

--- local_refinement.m
+++ local_refinement.m
@@ -1,8 +1,6 @@
 import sizing_fucntion.m
-import mesh.m
 
-
-%creates matrix that stores global edge number for every global node number
+%creates matrix that stores global edge number for every global element number
 %
 %I2E = interior edge matrix storing each local edge number for each node
 %B2E = boundary edge matrix storing each local edge number for each node
@@ -60,13 +58,52 @@ function X_edge_midpt = edge_midpoint(Xpt1, Xpt2)
     X_edge_midpt = (Xpt1 + Xpt2) ./ 2;
 end
 
-%TODO takes a edge length and compares it to the sizing function
+%takes a edge length and compares it to the sizing function
 %
-%X_edge_midpt = midpoint coordinate of hte desired edge
-%h_edge = current edge length of desired edge
+%node_data = point cordinates of each global node
+%IE = nodes between global interior edges
+%BE = nodes between global boundary edges
 %X_BE_spline = splined boundary edge used for the shape function
-function flagged = flag_edge(X_edge_midpt, h_edge, X_BE_spline)
-    flagged = false;
+function [Iflagged, Bflagged] = flag_edge(node_data, IE, BE, X_BE_spline)
+    %initialize the flagged array for every edge
+    Iflagged = false(size(IE, 1));
+    Bflagged = false(size(BE, 1));
+
+    %loop over the interior edges
+    for i = 1:length(IE)
+        %get the nodes that make up an edge 
+        [n1, n2, elem1, elem2] = IE(i);
+        Xpt1 = node_data(n1); Xpt2 = node_data(n2);
+
+        %get the lengths of a current edge
+        h_current = norm(Xpt2 - Xpt1);
+        X_midpt = edge_midpoint(Xpt1, Xpt2);
+        h_desired = sizing_function.sizing(X_midpt, X_BE_spline);
+
+        %ensure the edge length is below the needed amount
+        if h_desired < h_current
+            Iflagged(i) = true;
+        end
+    end
+
+    %loop over the boundary edges
+    for i = 1:length(BE)
+        %get the nodes that make up an edge 
+        [n1, n2, elem] = BE(i);
+        Xpt1 = node_data(n1); Xpt2 = node_data(n2);
+
+        %get the lengths of a current edge
+        h_current = norm(Xpt2 - Xpt1);
+        X_midpt = edge_midpoint(Xpt1, Xpt2);
+        h_desired = sizing_function.sizing(X_midpt, X_BE_spline);
+
+        %ensure the edge length is below the needed amount
+        if h_desired < h_current
+            Bflagged(i) = true;
+        end
+
+    end
+
     h_desired = sizing_function.sizing(X_edge_midpt, X_BE_spline);
 
     if h_desired < h_edge
@@ -101,4 +138,43 @@ function Xsnew = smooth(Xs, N, isBoundary, num_iter)
         end
         Xs = Xsnew;
     end
+end
+
+%finds all boundary edges to avoid for smoothing
+%
+%B2E = matrix that stores the elemetns for each global boundary edge
+%E2N = stores 3 nodes for each element
+%num_nodes = number of nodes in the mesh
+function isBoundary = build_boundary_nodes(B2E, E2N, num_nodes)
+
+    isBoundary = false(num_nodes,1);
+    
+    % local face → local node indices
+    faceNodes = [2 3;
+                 3 1;
+                 1 2];
+    
+    for k = 1:size(B2E,1)
+    
+        elem = B2E(k,1);
+        face = B2E(k,2);
+    
+        % global nodes of this element
+        tri = E2N(elem,:);
+    
+        % nodes on this boundary edge
+        n1 = tri(faceNodes(face,1));
+        n2 = tri(faceNodes(face,2));
+    
+        isBoundary(n1) = true;
+        isBoundary(n2) = true;
+    end
+end %written by Chat-GPT
+
+function insertNode()
+    %TODO
+end
+
+function createEdge()
+    %TODO
 end
