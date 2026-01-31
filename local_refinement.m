7b52f9b75fdb3d4d6739224ad949da9a23b1b842
landinjm <landinjm@umich.edu>
2026-01-31 02:12:43 -0500

    partial local refinement rewrite

--- local_refinement.m
+++ local_refinement.m
@@ -6,7 +6,7 @@ function [new_nodes, new_elements] = local_refinement(nodes, elements,...
     n_interior_faces = size(I2E, 1);
 
     edge_refinement = zeros(n_elements, 3);
-
+    interior_edge_refinement = zeros(n_interior_faces, 1);
     % Loop over the interior faces to determine whether to flag refinement
     % or not
     for i = 1:n_interior_faces
@@ -33,32 +33,99 @@ function [new_nodes, new_elements] = local_refinement(nodes, elements,...
         if edge_length < ideal_h
             edge_refinement(left_element, left_face) = 1;
             edge_refinement(right_element, right_face) = 1;
+            interior_edge_refinement(i) = 1;
         end
     end
 
     % Initialize new nodes and elements
+    edge2node = containers.Map('KeyType','char','ValueType','int32');
     new_nodes = nodes;
-    new_elements = elements;
+    edge_key = @(a,b) sprintf('%d_%d', min(a,b), max(a,b));
+    function m = get_mid(a,b)
+        key = sprintf('%d_%d', min(a,b), max(a,b));
+        if isKey(edge2node,key)
+            m = edge2node(key);
+        else
+            m = [];
+        end
+    end
 
     % Update the nodes and elements with the refinement
-    for i = 1:n_elements
-        if any(edge_refinement(i, :))
-            % Calculate new node positions and add them to new_nodes
-            for j = 1:3
-                if edge_refinement(i, j) == 1
-                    local_nodes = [1 2 3];
-                    local_nodes(j) = [];
-                    new_node_position = (nodes(elements(i, local_nodes(1)), :) ...
-                        + nodes(elements(i, local_nodes(2)), :)) / 2
-                end
+    for i = 1:n_interior_faces
+        if interior_edge_refinement(i) == 1
+            eL = I2E(i,1);
+            fL = I2E(i,2);
+
+            nodesL = elements(eL,:);
+            nodesL(fL) = [];
+    
+            a = nodesL(1);
+            b = nodesL(2);
+    
+            key = edge_key(a,b);
+    
+            if ~isKey(edge2node, key)
+                midpoint = 0.5*(nodes(a,:) + nodes(b,:));
+                new_nodes(end+1,:) = midpoint;
+                edge2node(key) = size(new_nodes,1);
             end
         end
     end
 
+    new_elements = [];
+
+    for e = 1:n_elements
+        nodes_e = elements(e,:);
+        flags   = edge_refinement(e,:);
     
-end
-
-function smoother(point, adjacent_nodes)
-    iterations = 5;
-
+        n1 = nodes_e(1);
+        n2 = nodes_e(2);
+        n3 = nodes_e(3);
+    
+        % Look up midpoints if they exist
+        m12 = get_mid(n1,n2);
+        m23 = get_mid(n2,n3);
+        m31 = get_mid(n3,n1);
+    
+        nflag = sum(flags);
+    
+        switch nflag
+    
+            case 0
+                new_elements(end+1,:) = nodes_e;
+    
+            case 1
+                if flags(1)
+                    new_elements(end+1,:) = [n1 m12 n3];
+                    new_elements(end+1,:) = [m12 n2 n3];
+                elseif flags(2)
+                    new_elements(end+1,:) = [n1 n2 m23];
+                    new_elements(end+1,:) = [n1 m23 n3];
+                else
+                    new_elements(end+1,:) = [n1 n2 m31];
+                    new_elements(end+1,:) = [m31 n2 n3];
+                end
+    
+            case 2
+                if ~flags(1)
+                    new_elements(end+1,:) = [n1 n2 m23];
+                    new_elements(end+1,:) = [n1 m23 m31];
+                    new_elements(end+1,:) = [m31 m23 n3];
+                elseif ~flags(2)
+                    new_elements(end+1,:) = [n1 m12 n3];
+                    new_elements(end+1,:) = [m12 n2 m31];
+                    new_elements(end+1,:) = [m31 n2 n3];
+                else
+                    new_elements(end+1,:) = [n1 m12 m23];
+                    new_elements(end+1,:) = [m12 n2 n3];
+                    new_elements(end+1,:) = [m23 m31 n3];
+                end
+    
+            case 3
+                new_elements(end+1,:) = [n1 m12 m31];
+                new_elements(end+1,:) = [m12 n2 m23];
+                new_elements(end+1,:) = [m31 m23 n3];
+                new_elements(end+1,:) = [m12 m23 m31];
+        end
+    end
 end
