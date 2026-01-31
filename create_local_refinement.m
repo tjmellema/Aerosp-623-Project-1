e1ab906a21a1d07d36eefe5da4701f837333572c
tjmellema <115052434+tjmellema@users.noreply.github.com>
2026-01-30 23:15:46 -0500

    Add files via upload

--- create_local_refinement.m
+++ create_local_refinement.m
@@ -12,4 +12,4 @@ bot_coords = node_data(bot_nodes,:);
 
 [X_BE_spline_top, X_BE_spline_bottom] = spline_boundary(top_coords, bot_coords, 3);
 [IE, BE] = BuildEdges(element_data);
-[nodes, E2N] = localRefinement(node_data,element_data,BE,IE, X_BE_spline_top, X_BE_spline_bottom);
+[nodes, E2N] = localRefinement(node_data,element_data,BE,IE, X_BE_spline_top, X_BE_spline_bottom, periodic_pairs);
