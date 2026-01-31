38c1ad1065623b7ac8eb1f650fbd5592cffb4857
landinjm <landinjm@umich.edu>
2026-01-31 01:14:08 -0500

    what i've got

--- src/create_local_refinement.m
+++ src/create_local_refinement.m
@@ -5,6 +5,9 @@
 % Read in the data
 [node_data, element_data, boundary_mappings, periodic_pairs] = read_gri("coarse.gri");
 
+% Grab mappings
+[I2E, B2E, In, Bn, Area] = mappings(node_data, element_data, boundary_mappings, periodic_pairs);
+
 % Grab the coordinates of the blades
 top_nodes = boundary_mappings(boundary_mappings(:,3) == 7, 1:2);
 bottom_nodes = boundary_mappings(boundary_mappings(:,3) == 8, 1:2);
@@ -15,7 +18,4 @@ bottom_coords = unique(node_data(bottom_nodes,:), 'rows');
 % Create the splines
 [spline_top, spline_bottom] = spline_boundary(top_coords, bottom_coords, 3);
 
-% Build the 
-[IE, BE] = BuildEdges(element_data);
-
-[nodes, E2N] = localRefinement(node_data,element_data,BE,IE, spline_top, spline_bottom, periodic_pairs);
+[nodes, E2N] = local_refinement(node_data, element_data, spline_top, spline_bottom, I2E, B2E);
