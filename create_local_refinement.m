a953a48cfa83d6ca8c2c590c687867e5f0c8b289
landinjm <landinjm@umich.edu>
2026-01-30 23:16:11 -0500

    blah

--- create_local_refinement.m
+++ create_local_refinement.m
@@ -1,14 +1,15 @@
-%local refinement call
+%
+% Local refinement script
+%
 
+% Read in the data
+[node_data, element_data, boundary_mappings, periodic_pairs] = read_gri("coarse.gri");
 
+top_nodes = boundary_mappings(boundary_mappings(:,3) == 7, 1:2);
+bottom_nodes = boundary_mappings(boundary_mappings(:,3) == 8, 1:2);
 
-[node_data, element_data, boundary_mappings, periodic_pairs] = read_gri("passage_coarse.gri");
-
-top_nodes = unique(boundary_mappings(boundary_mappings(:,3)==7,1:2));
-bot_nodes = unique(boundary_mappings(boundary_mappings(:,3)==8,1:2));
-
-top_coords = node_data(top_nodes,:);
-bot_coords = node_data(bot_nodes,:);
+top_coords = unique(node_data(top_nodes,:), 'rows');
+bot_coords = unique(node_data(bottom_nodes,:), 'rows');
 
 [X_BE_spline_top, X_BE_spline_bottom] = spline_boundary(top_coords, bot_coords, 3);
 [IE, BE] = BuildEdges(element_data);
